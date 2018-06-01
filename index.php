c350ddebb8ee07352ae29fed89cd6f29dee93237
Laxman <laxman@netscriptindia.com>
2018-06-01 17:55:54 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<link href="style.css" rel="stylesheet" type="text/css">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script type="text/javascript" src="lightslider.js"></script>
+<script type="text/javascript" src="main.js"></script>
+</head>
+<body>
+<div class="demo">
+    <ul id="light-slider">
+        <li>
+            <h3>1</h3>
+            <p>Hello</p>
+            <p>Hello, what the hell ?</p>
+            <p>Hello, lets put his long terxt.....so long and, so long</p>
+            <p>Hello</p>
+
+        </li>
+        <li>
+            <h3>First Slide</h3>
+            <p>Lorem ipsum Cupidatat quis pariatur anim.</p>
+            <p>Lorem ipsum Cupidatat quis pariatur anim.</p>
+            <p>Lorem ipsum Cupidatat quis pariatur anim.</p>
+        </li>
+        <li>
+            <h3>Second Slide</h3>
+            <p>Lorem ipsum Excepteur amet adipisicing fugiat velit nisi.</p>
+            <p>Lorem ipsum Cupidatat quis pariatur anim.</p>
+            <p>Lorem ipsum Cupidatat quis pariatur anim.</p>
+        </li>
+        <li>
+            <h3>Third Slide</h3>
+            <p>Lorem ipsum Cupidatat quis pariatur anim.</p>
+            <p>Lorem ipsum Cupidatat quis pariatur anim.</p>
+            <p>Lorem ipsum Cupidatat quis pariatur anim.</p>
+        </li>
+
+    </ul>
+</div>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+          $("#light-slider").lightSlider({
+              /*item: 1,
+              autoWidth: true,
+              slideMove: 1, // slidemove will be 1 if loop is true
+              slideMargin: 40,
+
+              addClass: '',
+              mode: "slide",
+              useCSS: true,
+              cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+              easing: 'linear', //'for jquery animation',////
+
+              speed: 400, //ms'
+              auto: true,
+              pauseOnHover: false,
+              loop: false,
+              slideEndAnimation: true,
+              pause: 2000,
+
+              keyPress: false,
+              controls: true,
+              prevHtml: '',
+              nextHtml: '',
+
+              rtl:false,
+              adaptiveHeight:false,
+
+              vertical:false,
+              verticalHeight:500,
+              vThumbWidth:100,
+
+              thumbItem:1,
+              pager: true,
+              gallery: false,
+              galleryMargin: 5,
+              thumbMargin: 5,
+              currentPagerPosition: 'middle',
+
+              enableTouch:true,
+              enableDrag:true,
+              freeMove:true,
+              swipeThreshold: 40,
+
+              responsive : [],
+
+              onBeforeStart: function (el) {},
+              onSliderLoad: function (el) {},
+              onBeforeSlide: function (el) {},
+              onAfterSlide: function (el) {},
+              onBeforeNextSlide: function (el) {},
+              onBeforePrevSlide: function (el) {}*/
+			  adaptiveHeight:true,
+        item:1,
+        slideMargin:0,
+        loop:true
+          });
+      });
